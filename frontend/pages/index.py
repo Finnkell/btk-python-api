732f36ed80528bf4ebb2cdde185dac378bd6214c
yuhigawa <pablostreiski@outlook.com>
2021-12-01 17:53:30 -0300

    ADD: front end register page

--- frontend/pages/index.py
+++ frontend/pages/index.py
@@ -4,6 +4,7 @@ from pages.risk_management_app import RiskManagementApp
 from pages.btk_predict_app import PredictionApp
 from pages.stock_monitor_app import StockMonitorApp
 from pages.login import LoginApp
+from pages.register import RegisterApp
 
 import requests
 import json
@@ -13,13 +14,14 @@ class BTKApp:
     def __init__(self, logged):
         if not logged:
             self.login = LoginApp()
-            return
+            self.regiter = RegisterApp()
 
     def home(self, st):
         page = None
 
         if st.session_state.is_logged == False:
             self.login.home(st=st)
+            # self.regiter.home(st=st)
 
         if st.session_state.is_logged:
             st.header('BTK App 🏄🏻‍♂️')
--- frontend/routes.py
+++ frontend/routes.py
@@ -8,6 +8,9 @@ st.set_page_config(
 
 if "is_logged" not in st.session_state:
     st.session_state.is_logged = False
+    
+if "w_register" not in st.session_state:
+    st.session_state.w_register = False
 
 pagina = BTKApp(st.session_state.is_logged)
 pagina.home(st=st)

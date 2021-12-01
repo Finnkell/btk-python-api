9607ba06def60d810c1eab7f7afa7c19254443e7
yuhigawa <pablostreiski@outlook.com>
2021-12-01 12:06:48 -0300

    FIX: Front Back Login route and requirement packes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.http import Http404
-
-
-def error_404(request, exception):
-    HttpResponse('Error handler content', status=404)
-
-
-handler404 = error_404
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),

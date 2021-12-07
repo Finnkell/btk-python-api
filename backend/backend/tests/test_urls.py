cba1651f8b96ff80c1618ad15e638082240bb305
Matheus Rocha <matheusrochacamarg@gmail.com>
2021-12-07 17:42:02 -0300

    add: Initial tests structure for routes backend

---
+++ backend/backend/tests/test_urls.py
@@ -0,0 +1,16 @@
+from django.http import response
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class TestBackendURLs(TestCase):
+  def test_setups_URL(self):
+    self.assertEquals(True, False)
+
+  def test_admin_URL(self):
+    self.assertEquals(True, False)
+
+  def test_auth_URL(self):
+    self.assertEquals(True, False)
+
+  def test_tools_URL(self):
+    self.assertEquals(True, False)

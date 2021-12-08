61094c3a11e2f264e217f976e90c46384cf623d8
Matheus Rocha <matheusrochacamarg@gmail.com>
2021-12-08 10:23:27 -0300

    TEST: URLs access testing

--- backend/tools/tests/test_urls.py
+++ backend/tools/tests/test_urls.py
@@ -1,10 +1,16 @@
 from django.http import response
 from django.test import TestCase, Client
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from tools.views import HomeView, MarkowitzView
 
 class TestToolsURL(TestCase):
+  def setUp(self):
+    self.root_url = reverse('Tools homepage')
+    self.markowitz_url = reverse('Tools Markowitz')
+
   def test_root_URL(self):
-    self.assertEquals(True, False)
+    self.assertEquals(resolve(self.root_url).func.view_class, HomeView)
 
   def test_markowitz_URL(self):
-    self.assertEquals(True, False)
+    self.assertEquals(resolve(self.markowitz_url).func.view_class, MarkowitzView)
--- test_backend.sh
+++
@@ -1,5 +0,0 @@
-cd backend
-python manage.py test authentication
-python manage.py test backend
-python manage.py test setups
-python manage.py test tools
